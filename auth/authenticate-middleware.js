c223fcdb04f8f6cbbf1632c5405d288d97efa0dd
IsabellaGuo <isabella.guofei@gmail.com>
2020-02-28 08:49:33 -0800

    Middleware is done.

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,27 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+//bring in the jwt library and the jwtSecret
+const jwt = require ('jsonwebtoken');
+const { jwtSecret } = require ('../config/secrets.js');
 
+//export the piece of middleware
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+ //authorization is a standard property on headers object
+  const { authorization } = req.headers
+
+  if (authorization) {
+    jwt.verity(authorization, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Invalid credentials."})
+      } else {
+        req.decodedToken = decodedToken
+
+        next()
+      }
+    })
+  } else {
+    res.status(400).json({ you: 'shall not pass!' });
+  }
+  
 };
---
+++ config/secrets.js
@@ -0,0 +1,5 @@
+//export the secret
+
+module.exports = {
+    jwtSecret: process.env.JWTKEY || 'is it secret, is it safe?'
+}
